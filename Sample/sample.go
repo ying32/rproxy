e0a9c860a36cd69b57c183fc20c3a71d91e5d3fa
ying32 <ying_33@live.cn>
2019-05-07 00:22:30 +0800

    修改例程增加TLS支持

--- Sample/sample.go
+++ Sample/sample.go
@@ -11,8 +11,16 @@ func main() {
 		fmt.Println(r)
 		w.Write([]byte("请求成功！"))
 	})
+	go func() {
+		fmt.Println("监听HTTPS 8089端口中...")
+		err := http.ListenAndServeTLS(":8089", "./cert.pem", "./key.pem", nil)
+		if err != nil {
+			fmt.Println("开启HTTPS服务端失败：", err)
+		}
+	}()
+
 	fmt.Println("监听HTTP 8085端口中...")
 	if err := http.ListenAndServe(":8085", nil); err != nil {
-		fmt.Println("服务器开启失败")
+		fmt.Println("开启HTTP服血端失败：", err)
 	}
 }
