e5149da94927b3ffbe45aef9deb84bfec209920f
ying32 <ying_33@live.cn>
2019-05-09 21:54:12 +0800

    修改build.bat添加不输出Windows资源文件参数

--- lzUI/build.bat
+++ lzUI/build.bat
@@ -1,5 +1,5 @@
 @echo off
-res2go -outmain false -outpath "../" -pause "ew"
+res2go -outmain false -outpath "../" -pause "ew" -outres false
 echo // +build gui>> $
 echo.>> $
 type "..\MainForm.go" >> $
